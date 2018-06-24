5faba8c04352d4082defecbca03619cf270690e6
Ant Cassetta <antcassetta@icloud.com>
2018-06-23 20:06:42 -0400

    just adding basic classes

---
+++ drinkinTarot/Card.py
@@ -0,0 +1,7 @@
+
+class Card(object):
+    """docstring for Card"""
+
+    def __init__(self, arg):
+        super(Card, self).__init__()
+        self.arg = arg
---
+++ drinkinTarot/Cocktail.py
@@ -0,0 +1,6 @@
+class Cocktail(object):
+    """docstring for Cocktail"""
+
+    def __init__(self, arg):
+        super(Cocktail, self).__init__()
+        self.arg = arg
